4bcf88ca4ea98af7cc70556a48533b3885386d1f
BhartiDew <158425425+BhartiDew@users.noreply.github.com>
2024-02-07 11:55:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+
+import views.Welcome;
+
+public class Main {
+    public static void main(String[] args) {
+        Welcome w =new Welcome();
+        do {
+            w.welcomeScreen();
+        } while (true);
+    }}
+
+/* 6. Which data types are used for storing the image and file in the database table?
+BLOB data type is used to store the image in the database. We can also store videos and audio by using the BLOB data type. It stores the binary type of data.
+CLOB data type is used to store the file in the database. It stores the character type of data.
+*/
+ 
